212ab0d92a4e89661478218d7f351a0350111343
Lucas Bernardino <lucasblameu@gmail.com>
2022-04-29 15:08:15 -0300

    Execício 1 terminado e proejto iniciado

---
+++ Exerc1/Piramide.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc1
+{
+    internal class Piramide
+    {
+        public int N { get; set; }
+
+        public Piramide(int n)
+        {
+            if (n <= 0)
+            {
+                throw new SystemException();
+            }
+            else
+            {
+                N = n;
+            }
+            
+                     
+        }
+
+        public void desenha(int n)
+        {
+            N = n;
+
+            for (int i = 1; i <= n; i++)
+            { 
+
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                for (int j = 1; j <= i - 1; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();                      
+                                             
+            }
+
+        }
+
+    }
+}
+
+
---
+++ Exerc1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Exerc1;
+
+
+
+    Console.WriteLine("Digite o valor de linhas da pirâmide: ");
+    int n = int.Parse(Console.ReadLine());
+
+try
+{
+    Piramide p = new Piramide(n);
+    p.desenha(n);
+}
+
+catch (SystemException)
+{
+    Console.WriteLine("o valor precisa ser maior que 1! ");
+}
