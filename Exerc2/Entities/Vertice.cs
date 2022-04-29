2c481309d12da63894f70c447fb2dc43ee2b1e8a
Lucas Bernardino <lucasblameu@gmail.com>
2022-04-29 19:07:01 -0300

    exercício 2 completo

---
+++ Exerc2/Entities/Vertice.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+namespace Exerc2
+{
+    internal class Vertice
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public Vertice(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+
+        public double Distancia()
+        {
+            double resultado = X - Y;
+
+            double DEuclidian = Math.Sqrt(Math.Pow(resultado, 2));
+
+            return DEuclidian;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Escolha a posição desejada para mover os pontos ");
+            Console.Write("Ponto X: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Ponto Y: ");
+            double y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Vertice vertice = new(x, y);
+
+        }
+
+        public void VerificaVertice()
+        {
+            if (X == Y || Y == X)
+            {
+                 Console.WriteLine("Os vértices são iguais!");
+                
+            }
+            else
+            {
+                 Console.WriteLine("Os vértices não são iguais!");
+              
+            }
+        }
+
+    }
+}
---
+++ Exerc2/Program.cs
@@ -0,0 +1,8 @@
+﻿using Exerc2;
+
+Vertice v = new Vertice(5, 5);
+
+Console.WriteLine(v.Distancia());
+v.VerificaVertice();
+v.Move();
+v.VerificaVertice();
